9e58501bdfa9818960bb54b80ecb71774acec0be
Paul Morganthall <paul@morganthall.com>
2017-02-08 01:22:48 -0500

    Collect histogram of all roles.

--- clesti.rb
+++ clesti.rb
@@ -5,19 +5,24 @@ ROLE = 2
 
 doc = Nokogiri::HTML(File.open("clesti.html"))
 
+all_roles = Hash.new(0)
+
 doc.css("tbody tr")[1..-1].each do |row|
   columns = row.css("td")
 
   name = columns[NAME].text.strip
   if name != ""
-    puts name
+    # puts name
   end
 
   if columns[ROLE]
-    role = columns[ROLE].text.strip
-    puts "   #{role}"
+    full_role = columns[ROLE].text.strip
+    abbr_role = full_role.rpartition(" ").first
+    all_roles[abbr_role] += 1
   end
 
 end
 
-__END__
+all_roles.sort.to_h.each do |role, count|
+  puts "#{count}, #{role}"
+end
