78b67e6c2ce0dfc2c5d24b3bde989c814155d192
Paul Morganthall <paul@morganthall.com>
2017-09-19 13:54:45 -0400

    Move role collector into a method.
    In preparation for Sinatra implementation.

--- clesti.rb
+++ clesti.rb
@@ -3,23 +3,25 @@ require "nokogiri"
 NAME = 0
 ROLE = 2
 
-doc = Nokogiri::HTML(File.open("tmp/clesti.html"))
+def collect_roles
+  doc = Nokogiri::HTML(File.open("tmp/clesti.html"))
 
-all_roles = Hash.new(0)
+  all_roles = Hash.new(0)
 
-doc.css("tbody tr")[1..-1].each do |row|
-  columns = row.css("td")
+  doc.css("tbody tr")[1..-1].each do |row|
+    columns = row.css("td")
 
-  name = columns[NAME].text.strip
-  if name != ""
-    # puts name
+    name = columns[NAME].text.strip
+    if name != ""
+      # puts name
+    end
+
+    next unless columns[ROLE]
+    role = columns[ROLE].text.strip
+    all_roles[role] += 1
   end
 
-  next unless columns[ROLE]
-  role = columns[ROLE].text.strip
-  all_roles[role] += 1
-end
-
-all_roles.sort.to_h.each do |role, count|
-  puts "#{count}, #{role}"
+  all_roles.sort.to_h.each do |role, count|
+    puts "#{count}, #{role}"
+  end
 end
